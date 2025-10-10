8b2494c4320a7ebc31d9b2dd019e346cb663f4a7
Matheus-Matias08 <matheus.matias@estudante.ifmt.edu.br>
2025-10-10 12:29:14 -0400

    Add files via upload

---
+++ Ordem de Serviço/src/controller/UsuarioDAO.java
@@ -0,0 +1,175 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import model.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import jdbc.ModuloConexao;
+import view.TelaLogin;
+import view.TelaPrincipal;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.awt.HeadlessException;
+
+/**
+ *
+ * @author clebe
+ */
+public class UsuarioDAO {
+    
+    private Connection conexao;
+    
+    public UsuarioDAO(){
+        this.conexao = ModuloConexao.conectar();
+    }
+    
+    //Metodo efetuaLogin
+    public void efetuaLogin(String usuario, String senha ) {
+       
+        try {
+
+            //1 passo - SQLa
+            String sql = "select * from tbusuarios where usuario = ? and senha = ?";
+            PreparedStatement stmt;
+            stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, usuario);
+            stmt.setString(2, senha);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                //Usuario logou
+                String perfil = rs.getString(6);
+                if (perfil.equals("Admin")){
+                    TelaPrincipal tela = new TelaPrincipal();
+                    tela.setVisible(true);
+                    tela.jMnItmUsuario.setEnabled(true);
+                    tela.jMnRelatorio.setEnabled(true);
+                    tela.jLblUsuario.setText(rs.getString(2));
+                } else {
+                    TelaPrincipal tela = new TelaPrincipal();
+                    tela.setVisible(true);
+                }
+            } else {
+                //Dados incorretos
+                JOptionPane.showMessageDialog(null, "Dados incorretos!");
+                new TelaLogin().setVisible(true);
+            }
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro : " + erro);
+        }
+
+    }
+    public void adicionarUsuario(Usuario obj){
+        try{
+            String sql = "insert into tbusuarios(iduser,usuario,fone,login,senha,perfil) values(?,?,?,?,?,?)";
+            conexao = ModuloConexao.conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, obj.getIdUser());
+            stmt.setString(2, obj.getUsuario());
+            stmt.setString(3, obj.getFone());
+            stmt.setString(4, obj.getLogin());
+            stmt.setString(5,obj.getSenha());
+            stmt.setString(6, obj.getPerfil());
+            
+            stmt.execute();
+            stmt.close();
+            JOptionPane.showMessageDialog(null, "Cadastrado com Sucesso!");
+            
+        }catch (SQLIntegrityConstraintViolationException el){
+            JOptionPane.showMessageDialog(null,"Login em uso.\nEscolha outro login");
+        }catch (HeadlessException | SQLException e){
+            JOptionPane.showMessageDialog(null,e);
+        }finally {
+            try{
+                conexao.close();
+            }catch(SQLException ex){
+                JOptionPane.showMessageDialog(null, ex);
+            }
+        }
+        
+    }
+    
+    public Usuario buscarUsuario(int id){
+        try{
+            String sql = "SELECT * FROM tbusuarios WHERE iduser = ?;";
+            conexao = ModuloConexao.conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()){
+                Usuario usuario = new Usuario();
+                usuario.setIdUser(rs.getInt("iduser"));
+                usuario.setUsuario(rs.getString("usuario"));
+                usuario.setFone(rs.getString("fone"));
+                usuario.setLogin(rs.getString("login"));
+                usuario.setSenha(rs.getString("senha"));
+                usuario.setPerfil(rs.getString("perfil"));
+                return usuario;
+            }
+            else{
+                JOptionPane.showMessageDialog(null,"Usuário não encontrado");
+            }
+            stmt.close();
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
+    public void alterarUsuario(Usuario obj){
+        try{
+            String sql = "UPDATE tbusuarios SET usuario = ?, fone = ?, login = ?, senha = ?, perfil = ? WHERE iduser = ?;";
+            conexao = ModuloConexao.conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            
+            stmt.setString(1, obj.getUsuario());
+            stmt.setString(2, obj.getFone());
+            stmt.setString(3, obj.getLogin());
+            stmt.setString(4,obj.getSenha());
+            stmt.setString(5, obj.getPerfil());
+            stmt.setInt(6, obj.getIdUser());
+            
+            stmt.execute();
+            stmt.close();
+            JOptionPane.showMessageDialog(null, "Usuário atualizado com Sucesso!");
+            
+        }catch (SQLIntegrityConstraintViolationException el){
+            JOptionPane.showMessageDialog(null,"Login em uso.\nEscolha outro login");
+        }catch (HeadlessException | SQLException e){
+            JOptionPane.showMessageDialog(null,e);
+        }finally {
+            try{
+                conexao.close();
+            }catch(SQLException ex){
+                JOptionPane.showMessageDialog(null, ex);
+            }
+        }
+    }
+    
+    public Usuario deleterUsuario(int id){
+        try{
+            String sql = "DELETE * FROM tbusuarios WHERE iduser = ?;";
+            conexao = ModuloConexao.conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()){
+               JOptionPane.showMessageDialog(null,"Usuário deleteado");
+            }
+            else{
+                JOptionPane.showMessageDialog(null,"Usuário não encontrado");
+            }
+            stmt.close();
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
+    
+}
